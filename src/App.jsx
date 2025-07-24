e9ca72c9d110b1121b9af552e691cf07800cf5f6
Brenda Sutara <sutarabrendagmail.com>
2025-07-24 16:18:16 -0300

    mesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
+import * as THREE from "three";
+
 function App() {
   return (
     <Canvas
@@ -9,10 +11,17 @@ function App() {
       }}
     >
       <OrbitControls />
-      <mesh>
+      <mesh position-x={-0.6}>
         <boxGeometry />
-        <meshNormalMaterial />
+        <meshStandardMaterial color="hotpink" side={THREE.FrontSide} />
       </mesh>
+      <mesh position-x={0.6} position-z={-1}>
+        <boxGeometry />
+        <meshStandardMaterial color="pink" side={THREE.DoubleSide} />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 3]} intensity={1} />
+      <directionalLight position={[0, 3, 3]} intensity={0.5} />
     </Canvas>
   );
 }
